5ed1f261aa7c70fc76b8a9c5f36ea41716ebeabe
Flavio Falcao <falcaorf@gmail.com>
2023-07-28 23:32:00 -0300

    Testando a primeira classe de dominio - Categoria

--- src/main/java/com/falcao/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/falcao/cursomc/resources/CategoriaResource.java
@@ -1,9 +1,15 @@
 package com.falcao.cursomc.resources;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.falcao.cursomc.domain.Categoria;
+
+
 
 @RestController
 @RequestMapping(value="/categorias")
@@ -12,8 +18,17 @@ public class CategoriaResource {
 	//@Autowired
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando!";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1,"Informática");
+		Categoria cat2 = new Categoria(2,"Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		//return "REST está funcionando!";
+		return lista;
 	}
 	
 
